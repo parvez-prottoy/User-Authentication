16598afefa0427f25fb90d1cbbce73b7891cab06
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-24 22:34:22 +0600

    added morgan, cors, cookie parser and express middleware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,15 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use([
+  morgan("dev"),
+  cors(),
+  cookieParser(),
+  express.json(),
+  express.urlencoded({ extended: true }),
+]);
 
 export default app;
